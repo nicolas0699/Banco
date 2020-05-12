c5e89adf326c310e1e37480bff789924ff533c12
nicolas0699 <64296100+nicolas0699@users.noreply.github.com>
2020-05-12 11:34:34 -0500

    Update datos.cpp

--- datos.cpp
+++ datos.cpp
@@ -2,66 +2,125 @@
 #include<string>
 #include <ctype.h> 
 #include "datos.hpp"
+#include <iomanip>
+#include <fstream>
 using namespace std;
 
-//Función que permite poner en mayusculas la informacion para asi evitar problemas a la hora de buscala
+int  idd=0;
+
 string Datos::mayusculas(string cadena){
     for(int i=0; i< cadena.size(); i++)
         cadena[i] = toupper(cadena[i]);
     return cadena;
 }
 
-//retorna el tamaño de la base de datos
 int Datos::tamano(){
     return count;
 }
 
-
-//inserta la informacón corerspondiente de cada donante 
 void Datos::insertar(string ident, string nom1, string nom2, string ape1, string ape2, string nac, string sexo, string rh, double dist){
-    int id=0;
     Info * informacion = new Info;
     if (paciente == nullptr){
-        informacion->id=id++;
+        informacion->id=idd +1;
         informacion->identifiacion=ident;
-        informacion->nombre1=nom1;
-        informacion->nombre2=nom2;
-        informacion->apellido1=ape1;
-        informacion->apellido2=ape2;
-        informacion->nacimiento=nac;
-        informacion->sexo=sexo;
-        informacion->rh=rh;
+        informacion->nombre1=mayusculas(nom1);
+        informacion->nombre2=mayusculas(nom2);
+        informacion->apellido1=mayusculas(ape1);
+        informacion->apellido2=mayusculas(ape2);
+        informacion->nacimiento=mayusculas(nac);
+        informacion->sexo=mayusculas(sexo);
+        informacion->rh=mayusculas(rh);
         informacion->distancia=dist;
         informacion->back=paciente;
         informacion->next=nullptr;
         paciente = informacion;
         count ++;
+        idd = informacion->id;
 
     }else{
-        informacion->id=id++;
-        informacion->identifiacion=ident;
-        informacion->nombre1=nom1;
-        informacion->nombre2=nom2;
-        informacion->apellido1=ape1;
-        informacion->apellido2=ape2;
-        informacion->nacimiento=nac;
-        informacion->sexo=sexo;
-        informacion->rh=rh;
+        informacion->id=idd +1 ;
+        informacion->identifiacion= ident;
+        informacion->nombre1=mayusculas(nom1);
+        informacion->nombre2=mayusculas(nom2);
+        informacion->apellido1=mayusculas(ape1);
+        informacion->apellido2=mayusculas(ape2);
+        informacion->nacimiento=mayusculas(nac);
+        informacion->sexo=mayusculas(sexo);
+        informacion->rh=mayusculas(rh);
         informacion->distancia=dist;
         informacion->next = paciente;
         paciente = informacion;
         informacion->back = paciente;
         count ++;
+        idd = informacion->id;
     }
 }
 
 
-# Realiza un display de la información para que pueda verla el usuario 
+
+string Datos::get_ident(int i){
+    Info * temp = encontrar(i);
+    return temp->identifiacion;
+}
+
+string Datos::get_nom1(int i){
+    Info * temp = encontrar(i);
+    return temp->nombre1;
+}
+
+string Datos::get_nom2(int i){
+    Info * temp = encontrar(i);
+    return temp->nombre2;
+}
+
+string Datos::get_apel1(int i){
+    Info * temp = encontrar(i);
+    return temp->apellido1;
+}
+
+string Datos::get_apel2(int i){
+    Info * temp = encontrar(i);
+    return temp->apellido2;
+}
+
+string Datos::get_nac(int i){
+    Info * temp = encontrar(i);
+    return temp->nacimiento;
+}
+
+string Datos::get_sexo(int i){
+    Info * temp = encontrar(i);
+    return temp->sexo;
+}
+
+string Datos::get_rh(int i){
+    Info * temp = encontrar(i);
+    return temp->rh;
+}
+
+double Datos::get_dist(int i){
+    Info * temp = encontrar(i);
+    return temp->distancia;
+}
+
+
+Info* Datos::encontrar(int i){
+    Info * temp = paciente;
+    while (temp != nullptr){
+        if(temp->id == i){
+            return temp;
+        }
+        temp = temp->next;
+    }
+    return nullptr;
+}
+
 void Datos::ver_pacientes(){
-    cout << "identificacion" << "   " <<  "primer nombre" << "   " << "segundo nombre" << "   " << "primer apellido" << "   " << "segundo apellido" << "   " << "fecha nacimiento" << "   " << "sexo"<< "   " <<  "rh" << "   " <<  "distancia" << endl;
-    Info *x = paciente;
-    for(int i=0; i < count; i++){
-        cout << x->identifiacion << "\t   " << mayusculas(x->nombre1) << "\t  " << mayusculas(x->nombre2) << "\t\t" << mayusculas(x->apellido1) << "  \t" << mayusculas(x->apellido2) << "\t\t" << mayusculas(x->nacimiento) << "\t    " << mayusculas(x->sexo) << "\t  " << mayusculas(x->rh) << "\t" << x->distancia << endl; 
-        x = x->next;
+    cout << setw(15) <<left << "ID" << setw(15) << left << "identificacion" << setw(15) << left <<   "Nombre1" << setw(15) << left << "Nombre2" << setw(15) << left << "Apellido1" << setw(15) << left << "Apellido2" << setw(15) << left << "Nacimiento" << setw(15) << left << "Sexo"<< setw(15) << left <<  "RH" << setw(15) << left <<  "Distancia" << endl;
+    for(int i=1; i <= count; i++){
+        cout << setw(15) << left << i << setw(15) << left << get_ident(i) << setw(15) << left << get_nom1(i) << setw(15) << left << get_nom2(i) << setw(15) << left << get_apel1(i) << setw(15) << left << get_apel2(i) << setw(15) << left << get_nac(i) << setw(15) << left << get_sexo(i) << setw(15) << left << get_rh(i) << setw(15) << left << get_dist(i) << "\n"; 
     } 
 }
+
+
+
