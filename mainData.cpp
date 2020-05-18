ef75c4dce50efc88b66f55420817185627fc6899
nicolas0699 <64296100+nicolas0699@users.noreply.github.com>
2020-05-17 23:55:40 -0500

    Update mainData.cpp

--- mainData.cpp
+++ mainData.cpp
@@ -51,8 +51,9 @@ vector<string> match(string cad){
   }
   return matches;
 }
-/*Esta función lee la cadena que se le ingrese y la separa por sus respectivas palabras, por lo que los espacios quedan eliminados y cada dato es guardado en un arreglo eel cuál se utiliza para poder insertar en la estructura los pacientes que ya exisitian en el archivo txt*/
-
+/*la función in_datos() lee la cadena que se le ingrese y la separa por sus respectivas palabras, 
+por lo que los espacios quedan eliminados y cada dato es guardado en un arreglo en el cuál
+se utiliza para poder insertar en la estructura los pacientes que ya exisitian en el archivo .txt*/
 Datos in_datos(string str, Datos x){
     string arr [11];
     int i = 0;
@@ -68,6 +69,7 @@ Datos in_datos(string str, Datos x){
     return x;
 }
 
+/*La función mayusculas() resive una cadena de string y la retorna en mayusculas*/
 string mayusculas(string cadena){
     int size = cadena.size();
     for(int i=0; i< size; i++)
@@ -81,12 +83,13 @@ int main(){
     Datos x;
     string y;
 /*
-/*esta seccion del codigo crea el archivo de texto (cant_tipo.txt) donde se guardará la informacion
-de los tipos de sangre y la cantidad que hay en en el banco, solo se debe correr una vez
-despues se comenta y se corre el programa para que actualice los valores en el mismo archivo*/
+/*esta seccion del codigo crea los archivos de texto (cant_tipo.txt y datos.txt) donde se guardará la informacion
+de los tipos de sangre y la cantidad que hay en en el banco.*/
 
 //#################################################################################################################################################################################################################################################################################################################################################################################################################################################
-// Con el open ya no es necesario comentarlas, el programa puede correr sin problemas
+// la función open() revisará si el archivo de texto existe en el area de trabajo, si no existe, creará uno.
+
+
   int fd = open("cant_tipo.txt", O_RDONLY);
   if(fd < 0){
     vector<string> TS{"O-","O+","A-","A+","B-","B+","AB-","AB+"};
@@ -100,7 +103,6 @@ despues se comenta y se corre el programa para que actualice los valores en el m
     inss.close();
   }
 
-  // Con el open ya no es necesario comentarlas, el programa puede correr sin problemas 
   int fd1 = open("datos.txt", O_RDONLY);
   if(fd1 < 0){
       ofstream in("datos.txt",ios::app);
@@ -109,13 +111,15 @@ despues se comenta y se corre el programa para que actualice los valores en el m
       in.close();
   }
 //#################################################################################################################################################################################################################################################################################################################################################################################################################################################
+// Antes del menú, se introduce la información en el archivo .txt a la estructura que guarda la información del donante.
+  
     ifstream out("datos.txt");
     int i = 0;
     getline(out, y);
     getline(out, y);
 
 
-    //Esta parte de aqui es porque estaba sucediendo un error al llegar a la ultima linea del archivo porque era una linea que no tenia contenido, entonces lo que hace al llegar a la ultima linea es salir del while y antes de eso emplea la función para insertar los datos que estaban en el archivo.
+    //Se utiliza este while porque estaba sucediendo un error al llegar a la ultima linea del archivo dado que era una linea que no tenia contenido, entonces lo que hace al llegar a la ultima linea es salir del while y antes de eso emplea la función para insertar los datos que estaban en el archivo.
     do{
         getline(out, y);
         if(out.eof())
@@ -125,8 +129,7 @@ despues se comenta y se corre el programa para que actualice los valores en el m
     }while(out.good());
     out.close();
 
-
-    /*Aqui es donde es importante que si es la primera vez que se abre el archivo, es decir es un archivo totalmente en blanco, se ingrese un paciente para que funcione el programa, despues ya no habrá ningun lio en ingresar o ir a las otras 2 opciones*/
+ //Comienza el menú
     int g;
     bool t=true;
     while(t){
@@ -140,6 +143,8 @@ despues se comenta y se corre el programa para que actualice los valores en el m
           cout<<"================================================="<<'\n';
           cout<<"Introduzca 1 si quiere agregar una persona"<<"\n2 si desea corregir algun dato"<<'\n';
           cin>>g2;
+
+          //Se inserta un paciente a la estructura y luego se actualiza a la base de datos (datos.txt).
           if(g2 == 1){
               string ident;
               string nom1;
@@ -158,13 +163,13 @@ despues se comenta y se corre el programa para que actualice los valores en el m
               cout << "Introduzca el primer nombre: ";
               cin >> nom1;
 
-              cout << "Introduzca el segundo nombre: ";
+              cout << "Introduzca el segundo nombre (NA en caso de no tener): ";
               cin >> nom2;
 
               cout << "Introduzca el primer apellido: ";
               cin >> apellido1;
 
-              cout << "Introduzca el segundo apellido: ";
+              cout << "Introduzca el segundo apellido (NA en caso de no tener): ";
               cin >> apellido2;
 
               cout << "Introduzca la fecha de nacimiento (ej: 17/06/1999): ";
@@ -182,7 +187,7 @@ despues se comenta y se corre el programa para que actualice los valores en el m
               cout << "Introduzca la distancia promedio en kilometros de su casa al hospital (ej: 6): ";
               cin >> dist;
 
-              //Desde aqui comienza la modificación
+              //Si la identidad ya existe, entonces no se ingresa 
               int id = 0;
               for(int i = 1; i < x.tamano(); i++){
                 if(ident == x.get_ident(i))
@@ -202,103 +207,165 @@ despues se comenta y se corre el programa para que actualice los valores en el m
                 in.close();
               
 
-              /*Nueva actualizacion 14/06/2020*/
-              /*Al ingresar un  paciente, toma su RH y aumenta el inventario de ese RH en 1*/
-              HashBlood bodega;
-              vector<string> intr;
-              ifstream in1("cant_tipo.txt");
-              string line="";
-              if(in1.good()){
-                while(!in1.eof()){
-                  getline(in1,line);
-                  intr.push_back(line);
+              //Al ingresar un  paciente, toma su RH y aumenta el inventario de ese RH en 1
+                HashBlood bodega;
+                vector<string> intr;
+                ifstream in1("cant_tipo.txt");
+                string line="";
+                if(in1.good()){
+                  while(!in1.eof()){
+                    getline(in1,line);
+                    intr.push_back(line);
+                  }
                 }
-              }
-              in1.close();
-              for(int i=0;i<intr.size()-1;i++){
-                vector<string> save=split(intr[i]," ");
-                 bodega.insert(save[0],stoi(save[1]), 1);
-              }
-            bodega.insert(x.get_rh(x.tamano()), 1, 0);
-
-            /*Modifica el archivo de texto con los cambios*/
-            ofstream of("cant_tipo.txt",ios::trunc);
-            if(of.good()){
-              for(int i=0;i<8;++i){
-                of<<bodega.info(i)<<'\n';
-              }
-            }
-          of.close();
-          }else
-          {
-            HashBlood bodega;
-              vector<string> intr;
-              ifstream in1("cant_tipo.txt");
-              string line="";
-              if(in1.good()){
-                while(!in1.eof()){
-                  getline(in1,line);
-                  intr.push_back(line);
+                in1.close();
+                for(int i=0;i<intr.size()-1;i++){
+                  vector<string> save=split(intr[i]," ");
+                  bodega.insert(save[0],stoi(save[1]), 1);
                 }
-              }
-              in1.close();
-              for(int i=0;i<intr.size()-1;i++){
-                vector<string> save=split(intr[i]," ");
-                 bodega.insert(save[0],stoi(save[1]), 1);
-              }
-            bodega.insert(x.get_rh(id), 1, 0);
+                bodega.insert(x.get_rh(x.tamano()), 1, 0);
 
-            /*Modifica el archivo de texto con los cambios*/
-            ofstream of("cant_tipo.txt",ios::trunc);
-            if(of.good()){
-              for(int i=0;i<8;++i){
-                of<<bodega.info(i)<<'\n';
+                /*Modifica el archivo de texto con los cambios*/
+                ofstream of("cant_tipo.txt",ios::trunc);
+                if(of.good()){
+                  for(int i=0;i<8;++i){
+                    of<<bodega.info(i)<<'\n';
+                  }
+                }
+                of.close();
+
+              //Si la identidad existe, unicamente aumentará los inventarios del rh que se registro con la identidad existente
+              }else{
+                HashBlood bodega;
+                vector<string> intr;
+                ifstream in1("cant_tipo.txt");
+                string line="";
+                if(in1.good()){
+                  while(!in1.eof()){
+                    getline(in1,line);
+                    intr.push_back(line);
+                  }
+                }
+                in1.close();
+                for(int i=0;i<intr.size()-1;i++){
+                  vector<string> save=split(intr[i]," ");
+                  bodega.insert(save[0],stoi(save[1]), 1);
+                }
+                bodega.insert(x.get_rh(id), 1, 0);
+
+                /*Modifica el archivo de texto con los cambios*/
+                ofstream of("cant_tipo.txt",ios::trunc);
+                if(of.good()){
+                  for(int i=0;i<8;++i){
+                    of<<bodega.info(i)<<'\n';
+                  }
+                }
+                of.close();
               }
-            }
-          of.close();
-          }
-          
           }
 
           if(g2 == 2){
+            /*Al ingresar en esta parte, se podrá modificar los datos de las personas, por si hubo algún error al digitar la informacíon. Tambien se actualiza en la base de datos.*/
+            int count;
+            int p;
+            string val;
+            cout << "1->Identificacion\n2->Nombre1\n3->Nombre2\n4->Apellido1\n5->Apellido2\n6->Nacimiento\n7->telefono\n8->sexo\n9->RH\n10->Distancia\n";
+            cin >> count;
+            if(count == 1){
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> val;
+              x.set_ident(p, val);
+              
+            }else if(count == 2){
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> val;
+              x.set_nom1(p, val);
+              
+            }else if(count == 3){
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> val;
+              x.set_nom2(p, val);
 
-              /*Esta parte del codigo modifica algún dato que pueda estar erroneo, aqui por simplicidad lo hice unicamente para el primer paciente, pero tambien se le podria solicitar al usuario cual paciente desea modificar pidiendole La ID del paciente y claramente se deberia preguntar al usuario por cual es el valor(string o double) que desea que se modifique. No desarrolle todo porque se va a cambiar el menú. Sin embargo, de esta forma se modificaria el archivo de texto.*/
+            }else if(count == 4){
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> val;
+              x.set_apel1(p, val);
 
-              int count;
-              cout << "1->Identificacion\n2->Nombre1\n3->Nombre2\n4->Apellido1\n5->Apellido2\n6->Nacimiento\n7->telefono\n8->sexo\n9->RH\n10->Distancia\n";
-              cin >> count;
-              if(count == 2)
-                  x.set_nom1(1, "Nico");
+            }else if(count == 5){
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> val;
+              x.set_apel2(p, val);
+                
+            }else if(count == 6){
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> val;
+              x.set_nac(p, val);
 
-              if(count == 3)
-                  x.set_nom2(1, "Alex");
+            }else if(count == 7){
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> val;
+              x.set_tel(p, val);
 
-              if(count == 4)
-                  x.set_apel1(1, "trujillo");
+            }else if(count == 8){
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> val;
+              x.set_sexo(p, val);
 
-              /* Cada vez que entre a este if, el archvio de texto debe modificarse, pues es la base de datos. El codigo que esta debajo de este comentario crea un archivo temporal para poder hacer la copia y modificar el archivo con los cambios realizados*/
+            }else if(count == 9){
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> val;
+              x.set_rh(p, val);
 
-              ifstream fs("datos.txt");
-              ofstream fstemp("datostemp.txt");
-
-              string line;
-
-              getline(fs, line);
-              fstemp << line << endl;
-              getline(fs, line);
-              fstemp << line << endl;
-
-              if(x.tamano() != 0){
-                  for(int i =1; i <= x.tamano(); i++){
-                      if(fstemp.good()){
-                          fstemp << setw(15) << left << i << setw(15) << left << x.get_ident(i) << setw(15) << left << x.get_nom1(i) << setw(15) << left << x.get_nom2(i) << setw(15) << left << x.get_apel1(i) << setw(15) << left << x.get_apel2(i) << setw(15) << left << x.get_nac(i) << setw(15) << left <<x.get_tel(i) << setw(15) << left << x.get_sexo(i) << setw(15) << left << x.get_rh(i)  << x.get_dist(i) << "\n";
-                      }
-                  }
+            }else if(count == 10){
+              double dist;
+              cout << "Indique la ID que desea cambiar: ";
+              cin >> p;
+              cout << "Escriba la información que reemplazará el dato: ";
+              cin >> dist;
+              x.set_dist(p, dist);
               }
-              fs.close();
-              fstemp.close();
-              remove("datos.txt");
-              rename("datostemp.txt", "datos.txt");
+
+             /* Cada vez que entre a este if, el archvio de texto debe modificarse, pues es la base de datos. El codigo que esta debajo de este comentario crea un archivo temporal para poder hacer la copia y modificar el archivo con los cambios realizados*/
+
+            ifstream fs("datos.txt");
+            ofstream fstemp("datostemp.txt");
+
+            string line;
+
+            getline(fs, line);
+            fstemp << line << endl;
+            getline(fs, line);
+            fstemp << line << endl;
+
+            if(x.tamano() != 0){
+                for(int i =1; i <= x.tamano(); i++){
+                    if(fstemp.good()){
+                        fstemp << setw(15) << left << i << setw(15) << left << x.get_ident(i) << setw(15) << left << x.get_nom1(i) << setw(15) << left << x.get_nom2(i) << setw(15) << left << x.get_apel1(i) << setw(15) << left << x.get_apel2(i) << setw(15) << left << x.get_nac(i) << setw(15) << left <<x.get_tel(i) << setw(15) << left << x.get_sexo(i) << setw(15) << left << x.get_rh(i)  << x.get_dist(i) << "\n";
+                    }
+                }
+            }
+            fs.close();
+            fstemp.close();
+            remove("datos.txt");
+            rename("datostemp.txt", "datos.txt");
           }
           string gg;
           cout<<"==================================="<<'\n';
